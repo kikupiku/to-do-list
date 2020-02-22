3babd73f63a1e1acfea940620876f4f97e55b682
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-02-22 01:38:46 +0100

    format task ui

--- src/task-ui.js
+++ src/task-ui.js
@@ -5,10 +5,11 @@ import { projects, sampleProject, renderProjects } from './project-ui.js';
 
 let sampleTask = taskFactory('Test Task', 'This is a short description',
                           '06/03/2020', 'Unassigned', 3);
+let sampleTask2 = taskFactory('Not so Urgent Task', 'Can be postponed, it\'s fine.',
+                          '08/03/2020', 'Unassigned', 2);
 
 sampleProject.tasks.push(sampleTask);
-console.log(sampleProject);
-console.log(projects);
+sampleProject.tasks.push(sampleTask2);
 
 const taskContainer = document.getElementById('task-container');
 
@@ -23,19 +24,26 @@ function renderTasks() {
       taskDiv.setAttribute('class', 'task-div');
       taskTitle.setAttribute('class', 'task-title');
       taskDescription.setAttribute('class', 'task-descr');
+      taskDeadline.setAttribute('class', 'task-deadline');
 
       taskTitle.innerHTML = element.title;
       taskDescription.innerHTML = element.description;
+      taskDeadline.innerHTML = 'Deadline: ' + element.deadline;
 
+      taskDiv.appendChild(taskDeadline);
       taskDiv.appendChild(taskTitle);
       taskDiv.appendChild(taskDescription);
       taskContainer.appendChild(taskDiv);
+
+      if (element.urgency === 1) {
+        taskDiv.style.border = '2px solid rgb(127,149,117)';
+      } else if (element.urgency === 2) {
+        taskDiv.style.border = '2px solid rgba(255,185,78,1)';
+      } else if (element.urgency === 3) {
+        taskDiv.style.border = '2px solid rgb(205,80,87)';
+      }
     });
   });
-}
-
-let taskColor = () => {
-  
 }
 
 export { renderTasks };
